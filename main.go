4869fd6e5ecea2dab4bf691ffadbeacc452a08dd
mtagizadee <magatagi0@gmail.com>
2023-07-09 16:09:40 +0400

    create company was added

--- main.go
+++ main.go
@@ -1,23 +1,42 @@
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+var db *gorm.DB
+
+func setDB(database *gorm.DB) {
+	db = database
+}
+
+func getDB(database *gorm.DB) *gorm.DB {
+	return db
+}
+
 func main() {
 	r := gin.Default()
 	api := r.Group("/api")
 	v1 := api.Group("/v1")
 
-	_, err := gorm.Open(mysql.Open("root:root@tcp(127.0.0.1:3306)/authdb?charset=utf8mb4&parseTime=True&loc=Local"))
+	db, err := gorm.Open(mysql.Open("root:root@tcp(127.0.0.1:3306)/authdb?charset=utf8mb4&parseTime=True&loc=Local"))
 	if err != nil {
 		panic("Could not connect to the database.")
 	}
+	setDB(db)
 	fmt.Println("Connected to the database.")
+
+	err = db.AutoMigrate(&company{})
+	if err != nil {
+		panic("Could not migrate the database.")
+	}
+	fmt.Println("Database migrated.")
 	
 	
 
@@ -35,9 +54,28 @@ type company struct {
 	gorm.Model
 	Name string `gorm:"not null" json:"name" binding:"required,min=2"`
 	Email string `gorm:"unique;not null" json:"email" binding:"required,email"`
-	Password string `gorm:"not null" json:"-" binding:"required,min=4,max=16"`
+	Password string `gorm:"not null" json:"password" binding:"required,min=4,max=16"`
+}
+
+func (c *company) hashPassword() {
+	hash := sha256.Sum256([]byte(c.Password))
+	c.Password = fmt.Sprintf("%x", hash)
 }
 
 func addCompany(c *gin.Context) {
-	
+		var company company;
+		if err := c.ShouldBindJSON(&company); err != nil {
+			c.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+		company.hashPassword()
+
+		db := getDB(db)
+		err := db.Create(&company).Error
+		if err != nil {
+			c.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusCreated, company)
 }
